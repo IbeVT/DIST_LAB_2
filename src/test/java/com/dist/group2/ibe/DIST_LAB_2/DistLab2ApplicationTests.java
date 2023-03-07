a18dba9b31b05f2120de8b2e8a9cc5823c12a0df
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-03-07 11:43:50 +0100

    Initial commit

---
+++ src/test/java/com/dist/group2/ibe/DIST_LAB_2/DistLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dist.group2.ibe.DIST_LAB_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistLab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
